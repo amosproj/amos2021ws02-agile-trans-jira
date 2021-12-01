0669ab75590b348af2d3c9d78a4d27d52ec4f1e6
Stefan Pawlowski <Stefan.Pawlowski@Infinithings.de>
2021-12-01 03:24:46 +0100

    Frappe Gantt as Ganntt view, CSS include not functional yet

--- frontend/src/js/components/DynamicTable.js
+++ frontend/src/js/components/DynamicTable.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import ReactDOM from "react-dom";
+import { FrappeGantt, Task, ViewMode } from 'frappe-gantt-react'
+
+const tasks = [
+  {
+    id: "Task 1",
+    name: "Redesign website",
+    start: "2016-12-28",
+    end: "2016-12-31",
+    progress: 10,
+    dependencies: "",
+  },
+  {
+    id: "Task 2",
+    name: "Redesign website",
+    start: "2016-12-28",
+    end: "2016-12-31",
+    progress: 20,
+    dependencies: "Task 1",
+  },
+  {
+    id: "Task 3",
+    name: "Redesign website",
+    start: "2016-12-28",
+    end: "2016-12-31",
+    progress: 0,
+    dependencies: "Task 2, Task 1",
+  },
+].map((x) => new Task(x));
 
 
 
 export default class DynamicTable extends React.Component {
 
+    constructor(props) {
+      super(props);
+      this.state = {
+        mode: ViewMode.Month,
+      };
+    }
+
   render() {
     return (
-      <div> Hello World!!! </div>
+      <div>
+        <FrappeGantt
+            tasks={tasks}
+            viewMode={this.state.mode}
+            onClick={task => console.log(task)}
+            onDateChange={(task, start, end) => console.log(task, start, end)}
+            onProgressChange={(task, progress) => console.log(task, progress)}
+            onTasksChange={tasks => console.log(tasks)}
+        />
+      </div>
     );
   }
 }
@@ -17,3 +61,4 @@ window.addEventListener('load', function() {
     const wrapper = document.getElementById("container");
     wrapper ? ReactDOM.render(<DynamicTable />, wrapper) : false;
 });
+
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,7 +11,18 @@ module.exports = {
             use: {
                 loader: "babel-loader"
             }
-          }
+          },
+          {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
         ]
     },
     watch: true,
