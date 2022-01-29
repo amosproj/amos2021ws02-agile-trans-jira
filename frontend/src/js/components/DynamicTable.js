2c2174ef43ae67f1cc272db13753ce28d7a46943
emiliakaf <emilia.kafedzhieva@campus.tu-berlin.de>
2022-01-29 13:43:32 +0100

    current state calendar

--- frontend/src/js/components/DynamicTable.js
+++ frontend/src/js/components/DynamicTable.js
@@ -2,53 +2,48 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import { FrappeGantt, Task, ViewMode } from 'frappe-gantt-react'
 
-const tasks = [
-  {
-    id: "Task 1",
-    name: "Redesign website",
-    start: "2021-10-28",
-    end: "2021-11-31",
-    progress: 10,
-    dependencies: "",
-  },
-  {
-    id: "Task 2",
-    name: "Redesign website",
-    start: "2021-11-31",
-    end: "2021-12-15",
-    progress: 20,
-    dependencies: "Task 1",
-  },
-  {
-    id: "Task 3",
-    name: "Redesign website",
-    start: "2021-12-15",
-    end: "2021-12-31",
-    progress: 0,
-    dependencies: "Task 2, Task 1",
-  },
-].map((x) => new Task(x));
+//const API = 'http://localhost:2990/jira/rest/api/2/search?jql=';
+//const DEFAULT_QUERY = 'assignee=currentUser()';
 
-
+//const tasks =  new Task();
 
 export default class DynamicTable extends React.Component {
 
     constructor(props) {
-      super(props);
-      this.state = {
-        mode: ViewMode.Month,
-      };
+        super(props);
+        this.state = {
+            mode: ViewMode.Month,
+            tasks: Task,
+        };
     }
 
-  render() {
+    componentDidMount() {
+        fetch('http://localhost:2990/jira/rest/api/2/search?jql&assignee=currentUser()', {
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => this.setState({tasks: data})); //
+        console.log(data)
+    }
+
+
+
+
+    render() {
+
     return (
       <div>
         <FrappeGantt
-            tasks={tasks}
+            tasks={
+                this.state.tasks}
             viewMode={this.state.mode}
             onClick={task => console.log(task)}
             onDateChange={(task, start, end) => console.log(task, start, end)}
-            onProgressChange={(task, progress) => console.log(task, progress)}
+            //onProgressChange={(task, progress) => console.log(task, progress)}
             onTasksChange={tasks => console.log(tasks)}
         />
       </div>
