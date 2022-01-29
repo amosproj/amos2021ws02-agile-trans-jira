5ee8d009783a6d67f72d33f87e86cd1fdfc45430
Roman K <romankupkovic1@gmail.com>
2022-01-29 18:25:22 +0100

    GET request for issue data; update state tasks with new issues; placeholder task still existing, can be deleted; used asynchronous function to get right order of fetching and updating state; not sure how the dom update acts together with function updateTasks();

--- frontend/src/js/components/DynamicTable.js
+++ frontend/src/js/components/DynamicTable.js
@@ -2,48 +2,98 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import { FrappeGantt, Task, ViewMode } from 'frappe-gantt-react'
 
-//const API = 'http://localhost:2990/jira/rest/api/2/search?jql=';
-//const DEFAULT_QUERY = 'assignee=currentUser()';
 
-//const tasks =  new Task();
+const startTask = [new Task(
+    {
+      id: "Task 2",
+      name: "Redesign website",
+      start: undefined,
+      end: undefined,
+      progress: 20,
+      dependencies: "",
+    },
+)]
+
 
 export default class DynamicTable extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            mode: ViewMode.Month,
-            tasks: Task,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: ViewMode.Month,
+      tasks: startTask,
     }
 
-    componentDidMount() {
-        fetch('http://localhost:2990/jira/rest/api/2/search?jql&assignee=currentUser()', {
-            method: 'GET',
-            headers: {
-                "Accept": "application/json",
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(response => response.json())
-            .then(data => this.setState({tasks: data})); //
-        console.log(data)
-    }
+  }
 
 
 
+  getIssuesPromise() {
+    return new Promise(async resolve => {
+      let newTaskArray = [];
+      let response = await fetch('http://localhost:2990/jira/rest/api/2/search?jql&assignee=currentUser()', {
+        method: 'GET',
+        headers: {
+          "Accept": "application/json",
+          'Content-Type': 'application/json'
+        }
+      })
+      let data = await response.json();
+      for (let obj in Array.from(data.issues)) {
+        let key = data.issues[obj].key
+        let name = data.issues[obj].fields.summary
+        let start = data.issues[obj].fields.customfield_10102
+        let end = data.issues[obj].fields.customfield_10103
+        console.log(key, name, start, end)
 
-    render() {
+        newTaskArray.push(new Task({
+          id: key,
+          name: name,
+          start: start,
+          end: end,
+          progress: 0,
+          dependencies: "",
+        }))
+      }
+      resolve(newTaskArray);
+    })
+  }
 
+
+  async updateTasks(){
+
+    let newTaskArray = await this.getIssuesPromise();
+
+    console.log("newTaskArray: " + newTaskArray)
+    console.log("state.tasks: ")
+    console.log(this.state.tasks)
+
+    let nextTasks = this.state.tasks;
+
+    nextTasks = nextTasks.concat(newTaskArray);
+
+    this.setState({
+      tasks: nextTasks,
+    })
+  }
+
+  componentDidMount() {
+    this.updateTasks();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    //this.updateTasks();
+  }
+
+  render() {
     return (
       <div>
         <FrappeGantt
-            tasks={
-                this.state.tasks}
+            tasks={this.state.tasks}
             viewMode={this.state.mode}
             onClick={task => console.log(task)}
             onDateChange={(task, start, end) => console.log(task, start, end)}
-            //onProgressChange={(task, progress) => console.log(task, progress)}
+            onProgressChange={(task, progress) => console.log(task, progress)}
             onTasksChange={tasks => console.log(tasks)}
         />
       </div>
