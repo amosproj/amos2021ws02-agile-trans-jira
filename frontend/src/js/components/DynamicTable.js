1253d68f64752152278922103ca4c613c9152804
Roman K <romankupkovic1@gmail.com>
2022-02-03 02:10:59 +0100

    merge fix wr-defs, package-lock.json

--- frontend/src/js/components/DynamicTable.js
+++ frontend/src/js/components/DynamicTable.js
@@ -1,52 +1,135 @@
 import React from 'react';
 import ReactDOM from "react-dom";
-import { FrappeGantt, Task, ViewMode } from 'frappe-gantt-react'
+import { FrappeGantt, Task, ViewMode } from 'frappe-gantt-react';
 
-const tasks = [
-  {
-    id: "Task 1",
-    name: "Redesign website",
-    start: "2021-10-28",
-    end: "2021-11-31",
-    progress: 10,
-    dependencies: "",
-  },
-  {
-    id: "Task 2",
-    name: "Redesign website",
-    start: "2021-11-31",
-    end: "2021-12-15",
-    progress: 20,
-    dependencies: "Task 1",
-  },
-  {
-    id: "Task 3",
-    name: "Redesign website",
-    start: "2021-12-15",
-    end: "2021-12-31",
-    progress: 0,
-    dependencies: "Task 2, Task 1",
-  },
-].map((x) => new Task(x));
 
+
+const startTask = [new Task(
+    {
+      id: "Task 2",
+      name: "Redesign website",
+      start: undefined,
+      end: undefined,
+      url: "",
+      progress:100,
+    },
+)]
 
 
 export default class DynamicTable extends React.Component {
 
-    constructor(props) {
-      super(props);
-      this.state = {
-        mode: ViewMode.Month,
-      };
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: ViewMode.Month,
+      tasks: startTask,
+    }
+    //this.handleClick = this.handleClick.bind(this);
+  }
+
+
+
+  getIssuesPromise() {
+    return new Promise(async resolve => {
+
+        let fieldsResponse = await fetch('http://localhost:2990/jira/rest/api/2/field',{
+            method: 'GET',
+            headers: {
+                "Accept": "application/json",
+                'Content-Type': 'application/json'
+            }
+        })
+        let fieldData= await fieldsResponse.json();
+        let startField;
+        let endField;
+
+        for (let data of fieldData) {
+            if (data.name == 'Start of Work' ) {
+                startField = data.id;
+            }
+            if (data.name == 'End of Work' ) {
+                endField = data.id;
+            }
+        };
+
+
+
+      let newTaskArray = [];
+      let response = await fetch('http://localhost:2990/jira/rest/api/2/search?jql=type%20%3D%20Request%20AND%20assignee%20%3D%20currentUser()%20AND%20status%20!%3D%20Done', {
+        method: 'GET',
+        headers: {
+          "Accept": "application/json",
+          'Content-Type': 'application/json'
+        }
+      })
+      let data = await response.json();
+      for (let obj in Array.from(data.issues)) {
+        let key = data.issues[obj].key
+        let name = data.issues[obj].fields.summary
+        let start = data.issues[obj].fields[startField]
+        let end = data.issues[obj].fields[endField]
+        //let url = data.issues[obj].self
+        //console.log(key, name, start, end, url)
+
+        newTaskArray.push(new Task({
+          id: key,
+          name: name,
+          start: start,
+          end: end,
+          url: window.location.protocol+'//' + window.location.host+'/jira/browse/'+key,
+          progress:100,
+        }))
+      }
+      resolve(newTaskArray);
+    })
+  }
+
+  async updateTasks(){
+
+    let newTaskArray = await this.getIssuesPromise();
+    console.log("working")
+    if(newTaskArray == this.state.tasks){
+      return
     }
 
+    console.log("newTaskArray: " + newTaskArray)
+    console.log("state.tasks: ")
+    console.log(this.state.tasks)
+
+
+
+    this.setState({
+      tasks: newTaskArray,
+    })
+  }
+
+  componentDidMount() {
+    this.updateTasks();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  //handleClick(task){
+  //  this.updateTasks()
+  //  console.log("clicked issue")
+  //  console.log(task.url)
+  //  window.location = task.url;
+  //}
   render() {
     return (
-      <div>
+        <div className="App">
+          <h1>Hello!</h1>
+          <h2>This is your personal request calendar.</h2>
         <FrappeGantt
-            tasks={tasks}
+            tasks={this.state.tasks}
             viewMode={this.state.mode}
-            onClick={task => console.log(task)}
+            onClick={task => {
+              this.updateTasks();
+              window.location=task.url;
+            }
+            }
             onDateChange={(task, start, end) => console.log(task, start, end)}
             onProgressChange={(task, progress) => console.log(task, progress)}
             onTasksChange={tasks => console.log(tasks)}
